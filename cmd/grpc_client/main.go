a565c4d18f8d8daa40bce1f56e301bb6bcfec2d6
DaniilShd <ds_const@mail.ru>
2024-03-24 20:55:55 +0300

    first commit2

--- cmd/grpc_client/main.go
+++ cmd/grpc_client/main.go
@@ -6,32 +6,42 @@ import (
 	"log"
 	"time"
 
-	desc "github.com/DaniilShd/gRPCExample/pkg/user_v1/api/user_v1"
+	desc "github.com/DaniilShd/gRPCtelegram/pkg/user_v1/api/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
 	address = "localhost:50051"
-	userID  = 12
 )
 
 func main() {
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Faild %s", err)
+		log.Fatalf("Faild1 %s", err)
 	}
 	defer conn.Close()
 
-	c := desc.NewUserV1Client(conn)
+	c := desc.NewSendMessageToTelegramClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	r, err := c.Get(ctx, &desc.GetRequest{Id: 16})
-	if err != nil {
-		log.Fatalf("Faild %s", err)
+	message := desc.TelegramMessage{
+		Id:      23,
+		ChantID: int64(379017783),
+		Text:    "hello!!!!!!",
 	}
 
-	fmt.Println(r)
+	var r *desc.SendResponse
+
+	for i := 0; i < 10; i++ {
+		r, err = c.Send(ctx, &desc.SendRequest{
+			MessageInfo: &message,
+		})
+		if err != nil {
+			log.Fatalf("Faild2 %s", err)
+		}
+		fmt.Println(r)
+	}
 }
--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -9,23 +9,25 @@ import (
 	desc "github.com/DaniilShd/gRPCtelegram/pkg/user_v1/api/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const grpcPort = 50051
 
 type server struct {
-	desc.UnimplementedUserV1Server
+	desc.UnimplementedSendMessageToTelegramServer
+	myBot *tgbotapi.BotAPI
 }
 
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("User id: %d", req.GetId())
+func (s *server) Send(ctx context.Context, req *desc.SendRequest) (*desc.SendResponse, error) {
 
-	return &desc.GetResponse{
-		Info: &desc.UserInfo{
-			Id:      req.GetId(),
-			Name:    "Daniil",
-			IsHuman: true,
-		},
+	msg := tgbotapi.NewMessage(req.MessageInfo.ChantID, req.MessageInfo.Text)
+	msg.ParseMode = "html"
+	s.myBot.Send(msg)
+
+	return &desc.SendResponse{
+		Check: true,
 	}, nil
 }
 
@@ -35,13 +37,50 @@ func main() {
 		log.Fatalf("Faild %s", err)
 	}
 
+	ch := make(chan *tgbotapi.BotAPI)
+
+	go StartTelegramBot(ch)
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterUserV1Server(s, &server{})
+	desc.RegisterSendMessageToTelegramServer(s, &server{
+		myBot: <-ch,
+	})
 
 	log.Printf("server listening")
 
 	if err = s.Serve(listen); err != nil {
 		log.Fatal("Error!")
+	}
+}
+
+func StartTelegramBot(ch chan *tgbotapi.BotAPI) {
+	bot, err := tgbotapi.NewBotAPI("6996608995:AAFKAsW2XjLEElrAgaeC8qPAzZOTocmlenA")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ch <- bot
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			fmt.Println(update.Message.Chat.ID)
+
+			bot.Send(msg)
+		}
 	}
 }
